93c1a8fc7d5935f00aea08236d1ed7a6fedca74b
E2515-Fatih <67655812+fatihseydi@users.noreply.github.com>
2020-10-25 23:45:13 +0300

    maven files

--- devops/hands-on/maven/maven_me/Hello.java
+++ devops/hands-on/maven/maven_me/Hello.java
@@ -3,7 +3,7 @@ public class Hello{
     public static void main(String[] arg) {
 
         //merhaba dünya...
-        System.out.println("Merhaba" + arg[0]);
+        System.out.println("Merhaba Dünya");
         
     }
 
